934867c87fe4423f699ce284fe63238dc23ec5c2
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2021-12-01 20:55:52 +0000

    refactor: day1

--- 1/sonar-sweep.js
+++ 1/sonar-sweep.js
@@ -10,30 +10,25 @@ function readFileLines(fileName) {
         resolve(data.split("\n"));
       }
     });
-  });
+  })
+    .then(R.map(R.trim))
+    .then(R.map(Number));
 }
 
 // readFileLines('./1/example1.txt')
 readFileLines("./1/input.txt")
-  .then(R.map(R.trim))
-  .then(R.filter(R.identity))
-  .then(R.map(Number))
-  // .then((x) => R.zip(x, R.drop(1, x)))
   .then(R.aperture(2))
-  .then(R.map((a) => a[0] < a[1]))
+  .then(R.map(R.apply(R.lt)))
   .then(R.filter(R.equals(true)))
   .then(R.length)
   .then(R.tap(console.log));
 
 // readFileLines("./1/example1.txt")
 readFileLines("./1/input.txt")
-  .then(R.map(R.trim))
-  .then(R.filter(R.identity))
-  .then(R.map(Number))
   .then(R.aperture(3))
   .then(R.map(R.sum))
   .then(R.aperture(2))
-  .then(R.map((a) => a[0] < a[1]))
+  .then(R.map(R.apply(R.lt)))
   .then(R.filter(R.equals(true)))
   .then(R.length)
   .then(R.tap(console.log));
