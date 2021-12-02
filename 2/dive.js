6796f70c7d822c625c28a6cf040946174198b0cc
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2021-12-02 08:56:20 +0000

    feat: day 2 part 2

--- 2/dive.js
+++ 2/dive.js
@@ -1,20 +1,46 @@
 const R = require("ramda");
 const fs = require('fs')
 
-// const data = fs.readFileSync('./example.txt', 'utf8')
-const data = fs.readFileSync('./input.txt', 'utf8')
-const transformed = R.pipe(
+const data = R.pipe(
   R.trim,
   R.split('\n'),
   R.map(R.split(' ')),
+  R.map(R.adjust(1, Number)),
+)(fs.readFileSync('./input.txt', 'utf8'))
+
+const part1 = R.pipe(
   R.groupBy(R.head),
   R.map(R.pluck(1)),
-  R.map(R.map(Number)),
   R.map(R.sum),
   R.tap(console.log)
 )(data)
 
-const depth = R.pathOr(0, ['down'], transformed) - R.pathOr(0, ['up'], transformed)
+const depth = R.pathOr(0, ['down'], part1) - R.pathOr(0, ['up'], part1)
 
 console.log('Depth ', depth);
-console.log('Result ', depth * R.pathOr(0, ['forward'], transformed))
+console.log('Result ', depth * R.pathOr(0, ['forward'], part1))
+
+console.log(data);
+const part2 = R.reduce(
+  (acc, cur) => {
+    const move = cur[0];
+    const val = cur[1];
+    if(move === 'down') {
+      return R.assoc('aim', acc['aim'] + val, acc);
+    }
+    if(move === 'up') {
+      return R.assoc('aim', acc['aim'] - val, acc);
+    }
+    if(move === 'forward') {
+      return R.pipe(
+       R.assoc('position', acc['position'] + val),
+       R.assoc('depth', acc['depth'] + acc['aim'] * val),
+      )(acc)
+    }
+
+    return acc;
+  },
+  { aim: 0, position: 0, depth: 0 }
+)(data)
+
+console.log(part2['position'] * part2['depth']);
